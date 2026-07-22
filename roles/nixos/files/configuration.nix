9c31988eece14b7232af99d53bb252ee0f53e654
Heorhi Sakovich <heorhi.sakovich@gmail.com>
2026-07-22 15:03:09 +0300

    chore: tune waybar

--- roles/nixos/files/configuration.nix
+++ roles/nixos/files/configuration.nix
@@ -78,4 +78,6 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.systemd-boot.configurationLimit = 5;
+
+  powerManagement.cpuFreqGovernor = "powersave";
 }
